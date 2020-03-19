7399e7c10a67b1c54967206389f94115cfe30a2e
hongtaoTang <hongtao.tang@my.jcu.edu.au>
2020-03-19 12:34:48 +0800

    create dashboard component

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function ProjectList() {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Clay Tang</p>
+          <p className="grey-text">1st January, 2pm</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Clay Tang</p>
+          <p className="grey-text">1st January, 2pm</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Clay Tang</p>
+          <p className="grey-text">1st January, 2pm</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectList;
