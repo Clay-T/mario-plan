1936615dae13c238cfef3366f4837db1cf68df50
CLAY <hongtao.tang@my.jcu.edu.au>
2020-03-19 14:44:01 +0800

    Create ProjectSummary component (#2)

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,31 +1,13 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 function ProjectList() {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Clay Tang</p>
-          <p className="grey-text">1st January, 2pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Clay Tang</p>
-          <p className="grey-text">1st January, 2pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Clay Tang</p>
-          <p className="grey-text">1st January, 2pm</p>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   )
 }
