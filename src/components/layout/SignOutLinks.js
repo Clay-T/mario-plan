eb310651ee5c74a8f363cc05a14caef8cef210d1
CLAY <hongtao.tang@my.jcu.edu.au>
2020-03-20 17:33:35 +0800

    navlink and css (#5)

--- src/components/layout/SignOutLinks.js
+++ src/components/layout/SignOutLinks.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 function SignOutLinks() {
   return (
     <ul className="right">
-      <li><NavLink to='/'>Sign Up</NavLink></li>
-      <li><NavLink to='/'>Login</NavLink></li>
+      <li><NavLink to='/signup'>Sign Up</NavLink></li>
+      <li><NavLink to='/signin'>Login</NavLink></li>
     </ul>
   )
 }
